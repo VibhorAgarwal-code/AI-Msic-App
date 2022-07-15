5b02148cc4a1ef85b0c0bd8b35d82e1807f0dcd4
VibhorAgarwal-code <85005341+VibhorAgarwal-code@users.noreply.github.com>
2022-07-15 20:52:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 ghungroo="";
 tune="";
 bts="";
+canvas=document.getElementById("canvas").value;
+
 function setup(){
-    canvas=createCanvas(600,500);
-    canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
+
+function preload(){
+    ghungroo=loadSound("Ghungroo - War.mp3");
+    tune=loadSound("music.mp3");
+    bts=loadSound("Butter.mp3");
 }
