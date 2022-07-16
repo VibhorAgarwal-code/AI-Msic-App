895279650395ea8a63cbe931313cc54110dbe5b0
VibhorAgarwal-code <85005341+VibhorAgarwal-code@users.noreply.github.com>
2022-07-16 18:13:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 ghungroo="";
-tune="";
 bts="";
-canvas=document.getElementById("canvas").value;
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 
 function setup(){
+    canvas=createCanvas(480,480);
+    canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet= ml5.poseNet(video, modelLoaded);
+   // poseNet.on('pose',gotPoses);
 }
 
 function draw(){
-    image(video,0,0,600,500);
+    image(video,0,0,480,480);
 }
 
 function preload(){
     ghungroo=loadSound("Ghungroo - War.mp3");
-    tune=loadSound("music.mp3");
     bts=loadSound("Butter.mp3");
+}
+function modelLoaded(){
+    console.log("Model is initialised");
 }
